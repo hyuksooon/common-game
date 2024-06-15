f61b8a70d51708726f4bf6e25b56e218cf6e307c
chs98412 <chs98412@gmail.com>
2024-06-15 20:28:32 +0900

    feat: #1 validation 추가

--- src/main/kotlin/com/soon/common/presentation/handler/model/ItemCreateRequest.kt
+++ src/main/kotlin/com/soon/common/presentation/handler/model/ItemCreateRequest.kt
@@ -1,10 +1,14 @@
 package com.soon.common.presentation.handler.model
 
 import com.soon.common.application.model.ItemCreateCommand
+import jakarta.validation.constraints.NotBlank
 
 data class ItemCreateRequest(
+        @field:NotBlank
         val title: String,
+        @field:NotBlank
         val description: String,
+        @field:NotBlank
         val thumbnail: String,
 ) {
     fun toCommand(serviceNo: Int) = ItemCreateCommand(
